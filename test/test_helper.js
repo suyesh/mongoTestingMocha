9e5e0ae79e308f93a6620f1d96dffb3f7b004ca6
Suyesh Bhandari <suyeshb@gmail.com>
2018-07-22 23:24:10 -0400

    Before callback

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/users_test', { useNewUrlParser: true });
-mongoose.connection
-        .once('open', () => console.log('Good to go!'))
-        .on('error', (error) => console.warn('Warning', error));
+before((done) => {
+  mongoose.connect('mongodb://localhost:27017/users_test', { useNewUrlParser: true });
+  mongoose.connection
+          .once('open', () => { done(); })
+          .on('error', (error) => console.warn('Warning', error));
+});
 
 beforeEach((done) => {
   mongoose.connection.collections.users.drop(() => {
