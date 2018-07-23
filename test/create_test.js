bbc4718e4f156c3ceed88b372544ed8356819bfb
Suyesh Bhandari <suyeshb@gmail.com>
2018-07-23 00:00:28 -0400

    Find user by _id

--- test/create_test.js
+++ test/create_test.js
@@ -4,7 +4,7 @@ const User = require('../src/models/user')
 describe('Creating records', () => {
   it('Saves a user', (done) => {
     const joe = new User({ name: 'Joe' });
-    
+
     joe.save()
        .then(() => {
          assert(!joe.isNew);
--- test/reading_test.js
+++ test/reading_test.js
@@ -8,14 +8,24 @@ describe('Reading records', () => {
   beforeEach((done) => {
     joe = new User({ name: 'Joe'});
     joe.save()
-       .then(() => done())
+       .then(() => {
+         done();
+       });
   });
 
   it('Finds all users with a name of Joe', (done) => {
     User.find({ name: 'Joe' })
         .then((users) => {
-          assert(users[0]._id.toString() === joe._id.toString())
+          assert(users[0]._id.toString() === joe._id.toString());
           done();
         });
+  });
+
+  it('find a user with a particular id', (done) => {
+    User.findOne({ _id: joe.id })
+        .then((user) => {
+          assert(user.name === 'Joe');
+          done();
+        })
   });
 })
