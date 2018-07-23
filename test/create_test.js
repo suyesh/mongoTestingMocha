bd8f4b5c3ed10e1571a6326ffda945b3c25be0f7
Suyesh Bhandari <suyeshb@gmail.com>
2018-07-22 23:21:16 -0400

    First commit

---
+++ test/create_test.js
@@ -0,0 +1,13 @@
+const assert = require('assert')
+const User = require('../src/models/user')
+
+describe('Creating records', () => {
+  it('Saves a user', (done) => {
+    const joe = new User({ name: 'Joe' });
+    joe.save()
+       .then(() => {
+         assert(!joe.isNew);
+         done();
+       });
+  });
+});
